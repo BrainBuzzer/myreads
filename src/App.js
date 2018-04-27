e97a0b12e8eade5ae314bf12e5a3f79358bf5762
Aditya Giri <dtrg21@gmail.com>
2018-04-27 16:25:09 +0530

    [feat] Add Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
+import './style/index.css'
+import Home from './components/Home'
+import Search from './components/Search'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+        <Route exact path="/" render={() =>
+          <Home />
+        } />
+        <Route exact path="/search" render={() =>
+          <Search />
+        } />
       </div>
     );
   }
