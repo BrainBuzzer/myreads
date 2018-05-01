4869147e8e5fba7c4ebcc54153ee708c688e8c2d
Aditya Giri <dtrg21@gmail.com>
2018-05-01 14:12:41 +0530

    [feat] Convert component to stateless component

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,61 +1,57 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import noCover from '../icons/no-cover-image.png'
 import StarRatingComponent from 'react-star-rating-component'
 
 /**
- * Card shown for single Book with details like rating and shelf.
- *
- * @class BookCard
- * @extends {Component}
+ * A special component for a single book.
  */
-class BookCard extends Component {
-  static propTypes = {
-    book: PropTypes.object.isRequired,
-    books: PropTypes.array.isRequired,
-    changeStatus: PropTypes.func.isRequired
-  }
-
-  render() {
-    const { book, books, changeStatus } = this.props
-    const coverImg = book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : noCover
-    let shelf = 'none'
-    for(let b of books) {
-      if(b.id === book.id) {
-        shelf = b.shelf
-      }
+const BookCard = (props) => {
+  const { book, books, changeStatus } = props
+  const coverImg = book.imageLinks && book.imageLinks.thumbnail ? book.imageLinks.thumbnail : noCover
+  let shelf = 'none'
+  for(let b of books) {
+    if(b.id === book.id) {
+      shelf = b.shelf
     }
-
-    return (
-      <div className="book">
-        <div className="book-img" style={{ backgroundImage: `url(${coverImg})` }} alt={book.title} />
-        <select
-          className="status"
-          onChange={(event) => changeStatus(book, event.target.value)}
-          value={shelf}>
-            <option value="none" disabled>Select an Option</option>
-            <option value="wantToRead">Want To Read</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-        </select>
-        <div className="book-name">{book.title}</div>
-        { book.averageRating && (
-          <div className="book-rating">
-            <StarRatingComponent
-              name="rating"
-              editing={false}
-              starCount={5}
-              value={book.averageRating}
-            />
-            <span>({book.ratingsCount})</span>
-          </div>
-        )}
-        <Link to={`/book/${book.id}`} className="button button-primary more-info">More Info</Link>
-      </div>
-    );
   }
+
+  return (
+    <div className="book">
+      <div className="book-img" style={{ backgroundImage: `url(${coverImg})` }} alt={book.title} />
+      <select
+        className="status"
+        onChange={(event) => changeStatus(book, event.target.value)}
+        value={shelf}>
+          <option value="none" disabled>Select an Option</option>
+          <option value="wantToRead">Want To Read</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+      </select>
+      <div className="book-name">{book.title}</div>
+      { book.averageRating && (
+        <div className="book-rating">
+          <StarRatingComponent
+            name="rating"
+            editing={false}
+            starCount={5}
+            value={book.averageRating}
+          />
+          <span>({book.ratingsCount})</span>
+        </div>
+      )}
+      <Link to={`/book/${book.id}`} className="button button-primary more-info">More Info</Link>
+    </div>
+  );
+}
+
+
+BookCard.propTypes = {
+  book: PropTypes.object.isRequired,
+  books: PropTypes.array.isRequired,
+  changeStatus: PropTypes.func.isRequired
 }
 
 export default BookCard
